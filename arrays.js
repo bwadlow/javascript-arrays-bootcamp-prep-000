a08dca9ebcf4b372f050d2bd47927c1c2bebc582
Brian Wadlow <bwadlow@gmail.com>
2018-06-12 15:43:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,12 @@
 function addElementToBeginningOfArray (arr, elem){
-  
+  let newArr=[elem, ...arr]
+  return newArr
 }
 
 
 function destructivelyAddElementToBeginningOfArray(arr, elem) {
-  
+  arr.unshift(elem)
+  return arr
 }
 
 
